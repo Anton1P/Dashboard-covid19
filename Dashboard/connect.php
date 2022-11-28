50ecad61bccd2f456d3f51f3f5faf047ab25c460
Antonin Parédé <118182733+Anton1P@users.noreply.github.com>
2022-11-28 20:00:29 +0100

    Dashboard Project Final
    The sql file : https://www.mediafire.com/file/r06vrayx8b0ez5w/db-paredant2.sql/file

---
+++ Dashboard/connect.php
@@ -0,0 +1,420 @@
+<?php 
+
+
+
+
+//connection bdd tables ;
+
+$table1="";
+$table2="";
+if (isset($table1) ) {$table1="donnees_45" ;}
+if (isset($table2) ) {$table2="indicateurs_45" ;}
+
+if ($sess=="sae303_45"){
+  $table1="donnees_45" ;
+  $table2="indicateurs_45" ;
+}
+else if ($sess=="sae303_01"){
+  $table1="donnees_01" ;
+  $table2="indicateurs_01" ;
+}
+else if ($sess=="sae303_02"){
+  $table1="donnees_02" ;
+  $table2="indicateurs_02" ;
+}
+else if ($sess=="sae303_03"){
+  $table1="donnees_03" ;
+  $table2="indicateurs_03" ;
+}
+else if ($sess=="sae303_04"){
+  $table1="donnees_04" ;
+  $table2="indicateurs_04" ;
+}else if ($sess=="sae303_05"){
+  $table1="donnees_05" ;
+  $table2="indicateurs_05" ;
+}
+else if ($sess=="sae303_06"){
+  $table1="donnees_06" ;
+  $table2="indicateurs_06" ;
+}
+else if ($sess=="sae303_07"){
+  $table1="donnees_07" ;
+  $table2="indicateurs_07" ;
+}
+else if ($sess=="sae303_08"){
+  $table1="donnees_08" ;
+  $table2="indicateurs_08" ;
+}
+else if ($sess=="sae303_09"){
+  $table1="donnees_09" ;
+  $table2="indicateurs_09" ;
+}
+
+//10
+else if ($sess=="sae303_10"){
+  $table1="donnees_10" ;
+  $table2="indicateurs_10" ;
+}
+else if ($sess=="sae303_11"){
+  $table1="donnees_11" ;
+  $table2="indicateurs_11" ;
+}
+else if ($sess=="sae303_12"){
+  $table1="donnees_12" ;
+  $table2="indicateurs_12" ;
+}
+else if ($sess=="sae303_13"){
+  $table1="donnees_13" ;
+  $table2="indicateurs_13" ;
+}
+else if ($sess=="sae303_14"){
+  $table1="donnees_14" ;
+  $table2="indicateurs_14" ;
+}
+else if ($sess=="sae303_15"){
+  $table1="donnees_15" ;
+  $table2="indicateurs_15" ;
+}else if ($sess=="sae303_16"){
+  $table1="donnees_16" ;
+  $table2="indicateurs_16" ;
+}
+else if ($sess=="sae303_17"){
+  $table1="donnees_17" ;
+  $table2="indicateurs_17" ;
+}
+else if ($sess=="sae303_18"){
+  $table1="donnees_18" ;
+  $table2="indicateurs_18" ;
+}
+else if ($sess=="sae303_19"){
+  $table1="donnees_19" ;
+  $table2="indicateurs_19" ;
+}
+else if ($sess=="sae303_19"){
+  $table1="donnees_19" ;
+  $table2="indicateurs_19" ;
+}
+//20
+else if ($sess=="sae303_20"){
+  $table1="donnees_20" ;
+  $table2="indicateurs_20" ;
+}
+else if ($sess=="sae303_21"){
+  $table1="donnees_21" ;
+  $table2="indicateurs_21" ;
+}
+else if ($sess=="sae303_22"){
+  $table1="donnees_22" ;
+  $table2="indicateurs_22" ;
+}
+else if ($sess=="sae303_23"){
+  $table1="donnees_23" ;
+  $table2="indicateurs_23" ;
+}
+else if ($sess=="sae303_24"){
+  $table1="donnees_24" ;
+  $table2="indicateurs_24" ;
+}else if ($sess=="sae303_25"){
+  $table1="donnees_25" ;
+  $table2="indicateurs_25" ;
+}
+else if ($sess=="sae303_26"){
+  $table1="donnees_26" ;
+  $table2="indicateurs_26" ;
+}
+else if ($sess=="sae303_27"){
+  $table1="donnees_27" ;
+  $table2="indicateurs_27" ;
+}
+else if ($sess=="sae303_28"){
+  $table1="donnees_28" ;
+  $table2="indicateurs_28" ;
+}
+else if ($sess=="sae303_29"){
+  $table1="donnees_29" ;
+  $table2="indicateurs_29" ;
+}
+//30
+else if ($sess=="sae303_30"){
+  $table1="donnees_10" ;
+  $table2="indicateurs_10" ;
+}
+else if ($sess=="sae303_31"){
+  $table1="donnees_31" ;
+  $table2="indicateurs_31" ;
+}
+else if ($sess=="sae303_32"){
+  $table1="donnees_32" ;
+  $table2="indicateurs_32" ;
+}
+else if ($sess=="sae303_33"){
+  $table1="donnees_33" ;
+  $table2="indicateurs_33" ;
+}
+else if ($sess=="sae303_34"){
+  $table1="donnees_34" ;
+  $table2="indicateurs_34" ;
+}else if ($sess=="sae303_35"){
+  $table1="donnees_35" ;
+  $table2="indicateurs_35" ;
+}
+else if ($sess=="sae303_36"){
+  $table1="donnees_36" ;
+  $table2="indicateurs_36" ;
+}
+else if ($sess=="sae303_37"){
+  $table1="donnees_37" ;
+  $table2="indicateurs_37" ;
+}
+else if ($sess=="sae303_38"){
+  $table1="donnees_38" ;
+  $table2="indicateurs_38" ;
+}
+else if ($sess=="sae303_39"){
+  $table1="donnees_39" ;
+  $table2="indicateurs_39" ;
+}
+//40
+else if ($sess=="sae303_40"){
+  $table1="donnees_40" ;
+  $table2="indicateurs_40" ;
+}
+else if ($sess=="sae303_41"){
+  $table1="donnees_41" ;
+  $table2="indicateurs_41" ;
+}
+else if ($sess=="sae303_42"){
+  $table1="donnees_42" ;
+  $table2="indicateurs_42" ;
+}
+else if ($sess=="sae303_43"){
+  $table1="donnees_43" ;
+  $table2="indicateurs_43" ;
+}
+else if ($sess=="sae303_44"){
+  $table1="donnees_44" ;
+  $table2="indicateurs_44" ;
+}
+else if ($sess=="sae303_46"){
+  $table1="donnees_46" ;
+  $table2="indicateurs_46" ;
+}
+else if ($sess=="sae303_47"){
+  $table1="donnees_47" ;
+  $table2="indicateurs_47" ;
+}
+else if ($sess=="sae303_48"){
+  $table1="donnees_48" ;
+  $table2="indicateurs_48" ;
+}
+else if ($sess=="sae303_49"){
+  $table1="donnees_49" ;
+  $table2="indicateurs_49" ;
+}
+//50
+else if ($sess=="sae303_50"){
+  $table1="donnees_50" ;
+  $table2="indicateurs_50" ;
+}
+else if ($sess=="sae303_51"){
+  $table1="donnees_51" ;
+  $table2="indicateurs_51" ;
+}
+else if ($sess=="sae303_52"){
+  $table1="donnees_52" ;
+  $table2="indicateurs_52" ;
+}
+else if ($sess=="sae303_53"){
+  $table1="donnees_53" ;
+  $table2="indicateurs_53" ;
+}
+else if ($sess=="sae303_54"){
+  $table1="donnees_54" ;
+  $table2="indicateurs_54" ;
+}
+else if ($sess=="sae303_55"){
+  $table1="donnees_55" ;
+  $table2="indicateurs_55" ;
+}
+else if ($sess=="sae303_56"){
+  $table1="donnees_56" ;
+  $table2="indicateurs_56" ;
+}
+else if ($sess=="sae303_57"){
+  $table1="donnees_57" ;
+  $table2="indicateurs_57" ;
+}
+else if ($sess=="sae303_58"){
+  $table1="donnees_58" ;
+  $table2="indicateurs_58" ;
+}else if ($sess=="sae303_59"){
+  $table1="donnees_59" ;
+  $table2="indicateurs_59" ;
+}
+//60
+else if ($sess=="sae303_60"){
+  $table1="donnees_60" ;
+  $table2="indicateurs_60" ;
+}
+else if ($sess=="sae303_61"){
+  $table1="donnees_61" ;
+  $table2="indicateurs_61" ;
+}
+else if ($sess=="sae303_62"){
+  $table1="donnees_62" ;
+  $table2="indicateurs_62" ;
+}
+else if ($sess=="sae303_63"){
+  $table1="donnees_63" ;
+  $table2="indicateurs_63" ;
+}
+else if ($sess=="sae303_64"){
+  $table1="donnees_64" ;
+  $table2="indicateurs_64" ;
+}
+else if ($sess=="sae303_65"){
+  $table1="donnees_65" ;
+  $table2="indicateurs_65" ;
+}
+else if ($sess=="sae303_66"){
+  $table1="donnees_66" ;
+  $table2="indicateurs_66" ;
+}
+else if ($sess=="sae303_67"){
+  $table1="donnees_67" ;
+  $table2="indicateurs_67" ;
+}else if ($sess=="sae303_68"){
+  $table1="donnees_68" ;
+  $table2="indicateurs_68" ;
+}
+else if ($sess=="sae303_69"){
+  $table1="donnees_69" ;
+  $table2="indicateurs_69" ;
+}
+//70
+else if ($sess=="sae303_70"){
+  $table1="donnees_70" ;
+  $table2="indicateurs_70" ;
+}
+else if ($sess=="sae303_71"){
+  $table1="donnees_71" ;
+  $table2="indicateurs_71" ;
+}
+else if ($sess=="sae303_72"){
+  $table1="donnees_72" ;
+  $table2="indicateurs_72" ;
+}
+else if ($sess=="sae303_73"){
+  $table1="donnees_73" ;
+  $table2="indicateurs_73" ;
+}
+else if ($sess=="sae303_74"){
+  $table1="donnees_74" ;
+  $table2="indicateurs_74" ;
+}
+else if ($sess=="sae303_75"){
+  $table1="donnees_75" ;
+  $table2="indicateurs_75" ;
+}
+else if ($sess=="sae303_76"){
+  $table1="donnees_76" ;
+  $table2="indicateurs_76" ;
+}else if ($sess=="sae303_77"){
+  $table1="donnees_77" ;
+  $table2="indicateurs_77" ;
+}
+else if ($sess=="sae303_78"){
+  $table1="donnees_78" ;
+  $table2="indicateurs_78" ;
+}
+else if ($sess=="sae303_79"){
+  $table1="donnees_79" ;
+  $table2="indicateurs_79" ;
+}
+//80
+else if ($sess=="sae303_80"){
+  $table1="donnees_80" ;
+  $table2="indicateurs_80" ;
+}
+else if ($sess=="sae303_81"){
+  $table1="donnees_81" ;
+  $table2="indicateurs_81" ;
+}
+else if ($sess=="sae303_82"){
+  $table1="donnees_82" ;
+  $table2="indicateurs_82" ;
+}
+else if ($sess=="sae303_83"){
+  $table1="donnees_83" ;
+  $table2="indicateurs_83" ;
+}
+else if ($sess=="sae303_84"){
+  $table1="donnees_84" ;
+  $table2="indicateurs_84" ;
+}
+else if ($sess=="sae303_85"){
+  $table1="donnees_85" ;
+  $table2="indicateurs_85" ;
+}
+else if ($sess=="sae303_86"){
+  $table1="donnees_86" ;
+  $table2="indicateurs_86" ;
+}
+else if ($sess=="sae303_87"){
+  $table1="donnees_87" ;
+  $table2="indicateurs_87" ;
+}
+else if ($sess=="sae303_88"){
+  $table1="donnees_88" ;
+  $table2="indicateurs_88" ;
+}else if ($sess=="sae303_89"){
+  $table1="donnees_89" ;
+  $table2="indicateurs_89" ;
+}
+//90
+else if ($sess=="sae303_90"){
+  $table1="donnees_90" ;
+  $table2="indicateurs_90" ;
+}
+else if ($sess=="sae303_91"){
+  $table1="donnees_91" ;
+  $table2="indicateurs_91" ;
+}
+else if ($sess=="sae303_92"){
+  $table1="donnees_92" ;
+  $table2="indicateurs_92" ;
+}
+else if ($sess=="sae303_93"){
+  $table1="donnees_93" ;
+  $table2="indicateurs_93" ;
+}
+else if ($sess=="sae303_94"){
+  $table1="donnees_94" ;
+  $table2="indicateurs_94" ;
+}
+else if ($sess=="sae303_95"){
+  $table1="donnees_95" ;
+  $table2="indicateurs_95" ;
+}
+else if ($sess=="sae303_971"){
+  $table1="donnees_971" ;
+  $table2="indicateurs_971" ;
+}
+else if ($sess=="sae303_972"){
+  $table1="donnees_972" ;
+  $table2="indicateurs_972" ;
+}
+else if ($sess=="sae303_973"){
+  $table1="donnees_973" ;
+  $table2="indicateurs_973" ;
+}else if ($sess=="sae303_974"){
+  $table1="donnees_974" ;
+  $table2="indicateurs_974" ;
+}
+else if ($sess=="sae303_976"){
+  $table1="donnees_976" ;
+  $table2="indicateurs_976" ;
+}
+
+
+?>
